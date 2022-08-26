eb883dd3a383e56b22b11231f3e61aa62bd2e2df
Goastio <seventylife@gmail.com>
2022-08-26 10:38:13 -0400

    css changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ const Nav = ({ setSubreddit }) => {
   const updateSubreddit = (e) => setSubreddit(e?.target?.value);
   const debouncedOnChange = debounce(updateSubreddit, 1000);
   return (
-    <div className="flex w-full p-2.5 bg-neutral-800">
+    <div className="flex w-full p-5 bg-neutral-800">
       <div className="flex justify-center w-full">
         <input
           type="text"
-          className="flex text-white rounded-md bg-white bg-opacity-10 p-2.5 hover:cursor-pointer focus:cursor-text focus:outline-white focus:outline-2 outline-none transition"
+          className="flex md:w-1/3 text-white rounded-md bg-white bg-opacity-10 p-2.5 hover:cursor-pointer focus:cursor-text focus:outline-white focus:outline-2 outline-none transition"
           onChange={debouncedOnChange}
         />
       </div>
